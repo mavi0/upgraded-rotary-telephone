caae57e2faee8c70b7fd8d9982ff9acba496434b
Eleanor Davies <ellie@mavieson.co.uk>
2019-10-16 13:54:54 +0100

    first try on pi

--- setup.sh
+++ setup.sh
@@ -18,13 +18,13 @@ sudo apt install -y python3 python iperf3 python-pip python3-pip speedtest-cli g
 sudo pip3 install iperf3 pingparsing jsonmerge
 sudo pip install yoctopuce
 
-## SSH setup
-echo -e "\e[33mSetting up SSH config \e[39m"
-cd ~/.ssh/
-wget "$1:5000/clone/authorized_keys"
-wget "$1:5000/clone/config"
-wget "$1:5000/clone/id_rsa"
-wget "$1:5000/clone/id_rsa.pub"
+# ## SSH setup
+# echo -e "\e[33mSetting up SSH config \e[39m"
+# cd ~/.ssh/
+# wget "$1:5000/clone/authorized_keys"
+# wget "$1:5000/clone/config"
+# wget "$1:5000/clone/id_rsa"
+# wget "$1:5000/clone/id_rsa.pub"
 
 ## Make Working directories
 echo -e "\e[33mMaking working directories \e[39m"
@@ -34,43 +34,43 @@ mkdir perf
 ls -al
 cd clone
 
-## Generate autossh service file
-echo -e "\e[33mgenerating AutoSSH service files \e[39m"
-FILE="autossh.service"
+# ## Generate autossh service file
+# echo -e "\e[33mgenerating AutoSSH service files \e[39m"
+# FILE="autossh.service"
 
-/bin/cat <<EOM >$FILE
-[Unit]
-Description=AutoSSH tunnel service
-After=network.target
+# /bin/cat <<EOM >$FILE
+# [Unit]
+# Description=AutoSSH tunnel service
+# After=network.target
 
-[Service]
-Environment="AUTOSSH_GATETIME=0"
-User=arisaie
-ExecStart=/usr/bin/autossh -N -F /home/arisaie/.ssh/config -i /home/arisaie/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $2:localhost:22 athena
+# [Service]
+# Environment="AUTOSSH_GATETIME=0"
+# User=arisaie
+# ExecStart=/usr/bin/autossh -N -F /home/arisaie/.ssh/config -i /home/arisaie/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $2:localhost:22 athena
 
-[Install]
-WantedBy=multi-user.target
+# [Install]
+# WantedBy=multi-user.target
 
-EOM
-cat autossh.service
+# EOM
+# cat autossh.service
 
 ## Install AutoSSH Service
-echo -e "\e[33mInstalling AutoSSH server files \e[39m"
-sudo cp -v autossh.service /etc/systemd/system/autossh.service
-sudo systemctl daemon-reload
-sudo systemctl enable autossh.service
-sudo systemctl start autossh.service
+# echo -e "\e[33mInstalling AutoSSH server files \e[39m"
+# sudo cp -v autossh.service /etc/systemd/system/autossh.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable autossh.service
+# sudo systemctl start autossh.service
 
-## Get HTTP server file
-echo -e "\e[33mGetting HTTP server service files \e[39m"
-wget "$1:5000/clone/simplehttp.service"
+# ## Get HTTP server file
+# echo -e "\e[33mGetting HTTP server service files \e[39m"
+# wget "$1:5000/clone/simplehttp.service"
 
-## Install HTTP Server Service
-echo -e "\e[33mInstalling HTTP server service files \e[39m"
-sudo cp -v simplehttp.service /etc/systemd/system/simplehttp.service
-sudo systemctl daemon-reload
-sudo systemctl enable simplehttp.service
-sudo systemctl start simplehttp.service
+# ## Install HTTP Server Service
+# echo -e "\e[33mInstalling HTTP server service files \e[39m"
+# sudo cp -v simplehttp.service /etc/systemd/system/simplehttp.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable simplehttp.service
+# sudo systemctl start simplehttp.service
 
 ## Generate Hostname var
 HOSTNAME= hostname
@@ -82,20 +82,20 @@ wget "$1:5000/clone/$HOSTNAME.ovpn"
 
 ## Install VPN service
 echo -e "\e[33minstalling VPN files \e[39m"
-sudo cp -v $HOSTNAME.ovpn /etc/openvpn/athena.conf
+sudo cp -v $HOSTNAME.ovpn /etc/openvpn/sixtimes.conf
 sudo systemctl daemon-reload
-sudo systemctl enable openvpn@athena.service
-sudo systemctl start openvpn@athena.service
+sudo systemctl enable openvpn@sixtimes.service
+sudo systemctl start openvpn@sixtimes.service
 
-## Configure Zabbix
-echo -e "\e[33mConfiguring Zabbix\e[39m"
-wget "$1:5000/clone/zabbix_agentd.conf"
+# ## Configure Zabbix
+# echo -e "\e[33mConfiguring Zabbix\e[39m"
+# wget "$1:5000/clone/zabbix_agentd.conf"
 
-sed -i "150s/.*/Hostname=$HOSTNAME/" zabbix_agentd.conf
+# sed -i "150s/.*/Hostname=$HOSTNAME/" zabbix_agentd.conf
 
-sudo cp -v zabbix_agentd.conf /etc/zaybbix/zabbix_agentd.conf
-sudo systemctl enable zabbix-agent
-sudo systemctl restart zabbix-agent
+# sudo cp -v zabbix_agentd.conf /etc/zaybbix/zabbix_agentd.conf
+# sudo systemctl enable zabbix-agent
+# sudo systemctl restart zabbix-agent
 
 ## Clone repos and install crontab
 
@@ -104,5 +104,5 @@ cd ~/perf
 git clone https://github.com/mavi0/upgraded-rotary-telephone.git
 sudo crontab upgraded-rotary-telephone/cronjobs
 
-echo -e "\e[33m$2\e[39m"
+# echo -e "\e[33m$2\e[39m"
 ifconfig
