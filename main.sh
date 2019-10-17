3feefeeb931a49066c79220b6da5a6570cf5c055
Eleanor Davies <ellie@mavieson.co.uk>
2019-10-17 13:07:20 +0100

    unsafe

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 export PATH=$PATH:~/.local/bin
 #ps aux |grep python |grep -v 'SimpleHTTPServer' |awk '{print $2}' |xargs kill
+mkdir iperfLogs
+mkdir pingLogs
+mkdir speedtestLogs
+
 killall python
 killall python3
 rm -f *.log
