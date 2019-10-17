9bbae60a44d919982edf312b956ecbb08b4a3fa3
Eleanor Davies <ellie@mavieson.co.uk>
2019-10-17 13:17:56 +0100

    safe

--- bp_send.py
+++ bp_send.py
@@ -2,8 +2,10 @@ import json, jsonmerge, requests, hashlib, configparser, socket
 
 
 def send_json(server_hostname, server_port, datafield, json_data): 
-    uuid = hashlib.md5(socket.gethostname().encode('utf-8')).hexdigest() 
-    res = requests.post('http://' + server_hostname + ':' + server_port + '/api/set_stats/' + uuid + '-' + datafield, json=json_data)
+    uuid = hashlib.md5(socket.gethostname().encode('utf-8')).hexdigest()
+    url = 'http://' + server_hostname + ':' + server_port + '/api/set_stats/' + uuid + '-' + datafield
+    print(url)
+    res = requests.post(url, json=json_data)
 
 def send_json_yocto():
     config = configparser.ConfigParser()
